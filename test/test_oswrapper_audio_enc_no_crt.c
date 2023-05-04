f35dc39f898933cde3579fbdfda106edc813ee1c
Ned Loynd <ned@loynd.com>
2023-05-04 18:57:58 +1000

    WMA support

--- test/test_oswrapper_audio_enc_no_crt.c
+++ test/test_oswrapper_audio_enc_no_crt.c
@@ -215,6 +215,12 @@ static OSWrapper_audio_enc_output_type demo_get_enum_for_str(const char* type) {
         return OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_MPEG;
     } else if (strcmp_impl(type, "mp3") == 0) {
         return OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_MP3;
+    } else if (strcmp_impl(type, "wma-lossless") == 0) {
+        return OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_WMA_LOSSLESS;
+    } else if (strcmp_impl(type, "wma-v9") == 0) {
+        return OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_WMA_V9;
+    } else if (strcmp_impl(type, "wma-v8") == 0 || strcmp_impl(type, "wma") == 0) {
+        return OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_WMA_V8;
     } else {
         return OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_WAV;
     }
@@ -235,6 +241,11 @@ static const char* demo_get_ext_for_enum(OSWrapper_audio_enc_output_type type) {
 
     case OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_MP3:
         return ".mp3";
+
+    case OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_WMA_V8:
+    case OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_WMA_V9:
+    case OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_WMA_LOSSLESS:
+        return ".wma";
 
     case OSWRAPPER_AUDIO_ENC_OUPUT_FORMAT_FLAC:
         return ".flac";
